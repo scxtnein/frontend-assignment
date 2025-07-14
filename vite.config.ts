33b3beac665e6184e7aba64817739d6cfe986450
scxtnein <scxtnein@gmail.com>
2025-07-15 07:19:32 +0800

    chore: init repo

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  // Ignore json-server db file changes
+  server: {
+    watch: {
+      ignored: ['**/clients.json'],
+    },
+  },
+});
